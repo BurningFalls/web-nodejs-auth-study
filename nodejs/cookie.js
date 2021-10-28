ae86ee63a5cff8023a7b397c28fdbbaa2cd427a2
spdlqj7791@naver.com <spdlqj7791@naver.com>
2021-10-28 10:42:35 +0900

    path & domain

--- nodejs/cookie.js
+++ nodejs/cookie.js
@@ -10,7 +10,9 @@ http.createServer(function(request, response) {
             'yummy_cookie=choco', 'tasty_cookie=strawberry',
             `Permanent=cookies; Max-Age=${60*60*24*30}`,
             'Secure=Secure; Secure',
-            'HttpOnly=HttpOnly; HttpOnly'
+            'HttpOnly=HttpOnly; HttpOnly',
+            'Path=Path; Path=/cookie',
+            'Domain=Domain; Domain=o2.org'
         ]
     });
     response.end('Cookie!!');
