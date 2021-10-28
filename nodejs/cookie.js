5ec3dd4e75779fda00900006ff0be2dd9a1a76cb
spdlqj7791@naver.com <spdlqj7791@naver.com>
2021-10-28 10:07:26 +0900

    create&read cookie

--- nodejs/cookie.js
+++ nodejs/cookie.js
@@ -1,4 +1,12 @@
-var http = require('http')
+var http = require('http');
+var cookie = require('cookie');
 http.createServer(function(request, response) {
+    var cookies = {};
+    if (request.headers.cookie !== undefined) {
+        cookies = cookie.parse(request.headers.cookie);
+    }
+    response.writeHead(200, {
+        'Set-Cookie':['yummy_cookie=choco', 'tasty_cookie=strawberry']
+    });
     response.end('Cookie!!');
 }).listen(3000);
